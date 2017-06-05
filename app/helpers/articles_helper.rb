1e9ccfbf49db30fe47c67e6a87e15754164c5b04
Michael Nagler <m.nagler995@gmail.com>
2017-06-05 14:24:41 -0400

    added paperclip gem

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   #require and permit example
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 end
